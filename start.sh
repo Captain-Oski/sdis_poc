75451642a8cbaf6b75a2d20ab0fe1532aef9f392
Captain-Oski <Captain-Oski@users.noreply.github.com>
2023-06-06 01:36:40 +0000

    updated running automation sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
-# Attempt to install h3 through pgnx 
-# Failing for now
-apt update &&
-apt install pip -y &&
-apt install cmake -y &&
-apt update &&
-pip install pgxnclient &&
+sudo docker compose run --rm ogr ogr2ogr -f PostgreSQL -lco GEOMETRY_NAME=geom -lco FID=gid -lco SPATIAL_INDEX=GIST -nln sdis.indice_emv -nlt PROMOTE_TO_MULTI -t_srs EPSG:3857-overwrite PG:"dbname='sdis' host='172.18.0.2' port='5432' user='admin_geo' password='password'"  ./data/indice_emv.shp \
+&&
+sudo docker compose run --rm ogr ogrinfo PG:"dbname='sdis' host='172.18.0.2' port='5432' user='admin_geo' password='password'" -sql @./data/1createRandomPointsInPolygons.sql \
+&&
+sudo docker compose run --rm ogr ogrinfo PG:"dbname='sdis' host='172.18.0.2' port='5432' user='admin_geo' password='password'" -sql @./data/2createGridAgainstPolygons.sql \
+&&
+sudo docker compose run --rm ogr ogrinfo PG:"dbname='sdis' host='172.18.0.2' port='5432' user='admin_geo' password='password'" -sql @./data/3createResults.sql
